78492f2928414f8cc48428d81f4f4137984a45d0
Edwin Mrtz <edwin.martinez.tech@gmail.com>
2020-11-16 21:21:17 -0800

    add 9 questions total

--- Develop/index.js
+++ Develop/index.js
@@ -20,11 +20,102 @@ const questions = [
                 return false;
             }
         }
-    },
-    //add another question
-    {
 
-    }
+    },
+    //add 2nd question to enter description of project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project',
+        //to validate answer
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("Please type a description of your project");
+                return false;
+            }
+        }
+    },
+    // enter 3rd question for installation instructions
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter installation instruction',
+        //to validate answer
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log("Please type instructions for installation");
+                return false;
+            }
+        }
+    },
+    // enter 4th question regarding usage information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter how to use application',
+        //to validate answer
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log("Please type how to use the application");
+                return false;
+            }
+        }
+    },
+    // enter 5th question regarding contribution guidelines
+    {
+        type: 'input',
+        name: 'guidelines',
+        message: "Please type guidelines for contribution",
+    },
+    // enter 6th question regarding test instructions
+    {
+        type: 'input',
+        name: 'test',
+        message: "Please enter instructions for testing",
+    },
+
+    //enter 7th question regarding github username
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please enter your Github username",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log("Please enter your Github username");
+                return false;
+            }
+        }
+    },
+    // enter 8th question regarding email address
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter your Email address",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter your Email address");
+                return false;
+            }
+        }
+    },
+    // enter 9th question regarding license
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please select a type of license for your project',
+        choices: ['MIT', 'GNU', 'ISC', 'Mozilla Public License', 'GPL', 'Affero GPL', 'Apache'],
+    },
+
 
 ];
 
